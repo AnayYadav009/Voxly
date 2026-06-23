f8dcc887e3f895241cad820a91b55db9d059df56
Anay <anayyadav009@gmail.com>
2026-06-23 08:38:50 +0530

    chore: unregister service worker to prevent caching of outdated assets

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { registerServiceWorker } from './serviceWorkerRegistration';
+import { unregisterServiceWorker } from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +12,8 @@ root.render(
   </React.StrictMode>
 );
 
-// Register service worker for offline support and push notifications
-registerServiceWorker();
+// Unregister service worker to prevent aggressive caching issues
+unregisterServiceWorker();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
